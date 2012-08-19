3f249c35f0ea2b25b95c6d7f6e71e317fd1ff96e
Andre Siviero <altsiviero@gmail.com>
2012-08-19 02:01:35 -0300

    Files to help prototyping added

---
+++ Ratings.m
@@ -0,0 +1,19 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%	Intuitionist Fuzzy Number Class
+%	
+%	Authors: André Pacheco and André Siviero
+%	Orienters: Renato Krohling and Rodolfo Lourenzutti
+%
+%	This file contains linguistic variables representing fuzzy sets, as if they were ranked from 0 to 10. These
+%	definitions are mostly for prototyping purposes, although they can be freely changed with no danger to the
+%	code itself.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+Very_Low = [0.1 1.2 2 2.8];
+Low = [2 3 4 5];
+Medium = [4 4.8 5.6 6.4];
+High = [6 6.8 7.6 8.4];
+Very_High = [7.6 8.4 9.2 10];
+
---
+++ testbench.m
@@ -0,0 +1,28 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%	Intuitionist Fuzzy Number Class
+%	
+%	Authors: André Pacheco and André Siviero
+%	Orienters: Renato Krohling and Rodolfo Lourenzutti
+%
+%	This file is used mostly as a draft space, for testing purposes on how to structure snippets of code. Most users
+%	don't need to refer to this file
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Load the ratings on Ratings.m
+Ratings
+
+D = [ IntuitionistFuzzyNumber(High,.7,.2) IntuitionistFuzzyNumber(Very_High)
+	IntuitionistFuzzyNumber(Very_High) IntuitionistFuzzyNumber(Low)
+	IntuitionistFuzzyNumber(High) IntuitionistFuzzyNumber(Medium)
+	IntuitionistFuzzyNumber(Low) IntuitionistFuzzyNumber(Very_Low) ];
+	
+CBVector = [1 0];
+
+IFM = IntuitionistFuzzyMatrix(CBVector,D);
+IFM.matrixD
+IFM.normalizeDecisionMatrix
+IFM.matrixD
+
+	
