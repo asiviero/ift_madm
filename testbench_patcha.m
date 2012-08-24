b6b1ad1783a593f3d80c9ab7f8c2e72fe6edd844
Andre Siviero <altsiviero@gmail.com>
2012-08-24 02:32:26 -0300

    TOPSIS added

---
+++ testbench_patcha.m
@@ -0,0 +1,39 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%	Intuitionist Fuzzy Number Class
+%	
+%	Authors: Andre Pacheco and Andre Siviero
+%	Orienters: Renato Krohling and Rodolfo Lourenzutti
+%
+%	This file is used mostly as a draft space, for testing purposes on how to structure snippets of code. Most users
+%	don't need to refer to this file
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Load the ratings on Ratings.m
+Ratings
+
+D = [ IntuitionistFuzzyNumber(Very_Low) IntuitionistFuzzyNumber(Medium)
+	IntuitionistFuzzyNumber(Very_High) IntuitionistFuzzyNumber(Low)
+	IntuitionistFuzzyNumber(High) IntuitionistFuzzyNumber(Medium)
+	IntuitionistFuzzyNumber(Medium) IntuitionistFuzzyNumber(Medium)
+	IntuitionistFuzzyNumber(Low) IntuitionistFuzzyNumber(Medium) ];
+	
+CBVector = [1 0];
+
+IFM = IntuitionistFuzzyMatrix(CBVector,D);
+IFM.matrixD;
+IFM.normalizeDecisionMatrix;
+%IFM.matrixD;
+
+%IFM.weights;
+%IFM.vectorW
+
+v = TOPSIS (IFM);
+
+
+
+
+
+
+	
