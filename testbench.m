00642fd780abac07e0edeb728f6b49b325a34814
Andre Siviero <altsiviero@gmail.com>
2012-08-23 22:07:49 -0300

    Aggregation added

--- testbench.m
+++ testbench.m
@@ -13,31 +13,41 @@
 % Load the ratings on Ratings.m
 Ratings
 
+vectorW = [IntuitionistFuzzyNumber(High) IntuitionistFuzzyNumber(Low)];
+vectorW2 = [IntuitionistFuzzyNumber(Medium) IntuitionistFuzzyNumber(Very_Low)];
+
+
 D = [ IntuitionistFuzzyNumber(Very_Low) IntuitionistFuzzyNumber(Medium)
 	IntuitionistFuzzyNumber(Very_High) IntuitionistFuzzyNumber(Low)
 	IntuitionistFuzzyNumber(High) IntuitionistFuzzyNumber(Medium)
-	IntuitionistFuzzyNumber(Extremely_Low) IntuitionistFuzzyNumber(Medium)
+	IntuitionistFuzzyNumber(Extremely_Low) IntuitionistFuzzyNumber(Medium)];
+
+D2 = [
 	IntuitionistFuzzyNumber(Extremely_High) IntuitionistFuzzyNumber(Medium)
 	IntuitionistFuzzyNumber(Medium) IntuitionistFuzzyNumber(Medium)
 	IntuitionistFuzzyNumber(Medium) IntuitionistFuzzyNumber(Medium)
-	IntuitionistFuzzyNumber(Low) IntuitionistFuzzyNumber(Medium)
-	IntuitionistFuzzyNumber(Extremely_Low) IntuitionistFuzzyNumber(Medium)
-	IntuitionistFuzzyNumber(Medium) IntuitionistFuzzyNumber(Medium)
-	IntuitionistFuzzyNumber(High) IntuitionistFuzzyNumber(Medium)
-	IntuitionistFuzzyNumber(Very_Low) IntuitionistFuzzyNumber(Medium)
-	IntuitionistFuzzyNumber(Medium) IntuitionistFuzzyNumber(Medium)
-	IntuitionistFuzzyNumber(Low) IntuitionistFuzzyNumber(Medium) ];
+	IntuitionistFuzzyNumber(Low) IntuitionistFuzzyNumber(Medium)];
+	%IntuitionistFuzzyNumber(Extremely_Low) IntuitionistFuzzyNumber(Medium)
+	%IntuitionistFuzzyNumber(Medium) IntuitionistFuzzyNumber(Medium)
+	%IntuitionistFuzzyNumber(High) IntuitionistFuzzyNumber(Medium)
+	%IntuitionistFuzzyNumber(Very_Low) IntuitionistFuzzyNumber(Medium)
+	%IntuitionistFuzzyNumber(Medium) IntuitionistFuzzyNumber(Medium)
+	%IntuitionistFuzzyNumber(Low) IntuitionistFuzzyNumber(Medium) ];
 	
 CBVector = [1 1];
 
-IFM = IntuitionistFuzzyMatrix(CBVector,D);
+IFM = IntuitionistFuzzyMatrix(CBVector,D,vectorW);
+IFM2 = IntuitionistFuzzyMatrix(CBVector,D2,vectorW2);
 
-IFM.matrixD
+
 IFM.normalizeDecisionMatrix
-IFM.matrixD
+IFM2.normalizeDecisionMatrix
 
-IFM.weights;
+IFM_V = [IFM IFM2]
 
-IFM.vectorW
+agg = aggregateFuzzyMatrixes(IFM_V,[.4 .6])
+agg.matrixD
+
+%IFM.vectorW = [IntuitionistFuzzyNumber(High) IntuitionistFuzzyNumber(Low)];
 
 	
